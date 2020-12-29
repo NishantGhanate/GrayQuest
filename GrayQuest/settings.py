704e3ae28669252cbd479f6292bff413daecb141
NishantGhanate <nishant7.ng@gmail.com>
2020-12-29 18:45:48 +0530

    staticfile path updated

--- GrayQuest/settings.py
+++ GrayQuest/settings.py
@@ -124,7 +124,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'guptastore'),
+    os.path.join(BASE_DIR, 'memeapp'),
 )
 
 MEDIA_URL = '/images/'
