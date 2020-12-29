a79577258f6e6fe6de0fffe5e0b1aa63a5784700
NishantGhanate <nishant7.ng@gmail.com>
2020-12-29 20:21:38 +0530

    updates requirements.txt

--- GrayQuest/settings.py
+++ GrayQuest/settings.py
@@ -12,9 +12,12 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 
 import os
 
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
+# import django_heroku
+# django_heroku.settings(locals())
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
